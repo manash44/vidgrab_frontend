c6183be51af8d37b2c57f96c4c9694341ac81a4c
manash44 <manashchariyadeori@gmail.com>
2026-02-18 19:15:06 +0530

    Configure production backend URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const APP_CONFIG = {
     name: "VidGetNow",
     subtitle: "Download video offline from anywhere",
     description: "The fast, free, and secure way to download videos.",
-    backendUrl: "http://localhost:5000"
+    backendUrl: "https://backend-iu1e.onrender.com"
 }
